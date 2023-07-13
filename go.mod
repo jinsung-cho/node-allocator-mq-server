b7aac6b9bfdcda33f7f2d761f23b64ee19f7ec74
jinsung-cho <cjs97111@gmail.com>
2023-07-13 13:11:53 +0900

    Initail commit for project

---
+++ go.mod
@@ -0,0 +1,14 @@
+module backend
+
+go 1.19
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/rabbitmq/amqp091-go v1.8.1 // indirect
+	github.com/rs/cors v1.9.0 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"backend/util"
+	"log"
+	"net/http"
+	"os"
+
+	"backend/controller"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
+)
+
+func main() {
+	log.Println("Server started on: server")
+
+	env_err := godotenv.Load(".env")
+	util.FailOnError(env_err, ".env Load fail")
+	hostIP := os.Getenv("HOST_IP")
+
+	r := mux.NewRouter()
+	corsConfig := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://" + hostIP + ":3000", "http://localhost:3000"},
+		AllowCredentials: true,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"*"},
+	})
+
+	handler := corsConfig.Handler(r)
+
+	r.HandleFunc("/yaml", controller.ParseYamlFile).Methods("POST")
+	r.HandleFunc("/run", controller.RunWorkflow).Methods("POST")
+
+	http.ListenAndServe(":8000", handler)
+}
