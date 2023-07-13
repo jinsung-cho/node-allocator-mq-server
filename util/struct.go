1a169d56244cd08a8eaff7db96841a125e0b54d9
jinsung-cho <cjs97111@gmail.com>
2023-07-13 14:15:18 +0900

    Fix: ContainerInfo struct

--- util/struct.go
+++ util/struct.go
@@ -1,10 +1,11 @@
 package util
 
 type ContainerInfo struct {
-	Name     string                 `json:"name"`
-	Image    string                 `json:"image"`
-	Limits   map[string]interface{} `json:"limits"`
-	Requests map[string]interface{} `json:"requests"`
+	Name         string                 `json:"name"`
+	Image        string                 `json:"image"`
+	Limits       map[string]interface{} `json:"limits"`
+	Requests     map[string]interface{} `json:"requests"`
+	NodeSelector map[string]interface{} `json:"nodeSelector"`
 }
 
 type Workflow struct {
