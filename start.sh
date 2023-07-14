07c6f2f296adacab0d00a159add61a825c157838
jinsung-cho <cjs97111@gmail.com>
2023-07-14 16:23:50 +0900

    Add: automation logic

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+export $(grep -v '^#' .env | xargs)
+
+# Kill processes
+fuser -k $GO_SERVER_PORT/tcp
+fuser -k $PYTHON_SERVER_PORT/tcp
+
+# RUN rabbitmq
+docker-compose -f ./rabbitmq/docker-compose.yaml up -d
 
 # RUN python flask server for argo workflow run
+pip3 install -r requirements.txt
 python3 argo_request_server.py &
 
 # RUN node allocator
