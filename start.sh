34459a21745130f8eb798604c635a4580ba60f6f
jinsung-cho <cjs97111@gmail.com>
2024-02-22 14:07:43 +0900

    fix: file path

--- start.sh
+++ start.sh
@@ -6,20 +6,25 @@ fuser -k $GO_SERVER_PORT/tcp
 fuser -k $PYTHON_SERVER_PORT/tcp
 
 # RUN rabbitmq
-docker-compose -f ./rabbitmq/docker-compose.yaml up -d
+docker-compose -f ./rabbitmq-docker/docker-compose.yaml up -d
 
 # RUN python flask server for argo workflow run
+cd ./argoIntegrationServer
 pip3 install -r requirements.txt
-python3 argo_request_server.py &
+python3 main.py &  
+cd ..
 
+pwd
 # RUN node allocator(go)
-cd nodeAllocator/goVersion 
-go run . &
+cd nodeAllocator/golang 
+go run . & 
+cd ../..
 
-# # RUN node allocator(python)
-# cd nodeAllocator/pythonVersion
-# python3 main.py &
+# RUN node allocator(python)
+cd nodeAllocator/python
+pip3 install -r requirements.txt
+python3 main.py &
 
 # RUN go backend server
 cd ../..
-go run . &
+go run . 
